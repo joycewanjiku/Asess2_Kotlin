42c4e84a0774460f5bab8a2a3ffdf730209224d1
student <joycemumbiiwanjiku@gmail.com>
2023-04-25 08:52:40 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,88 @@
+fun main() {
+    deposited(1000)
+    println(withdraw(500))
+
+}
+///1. Write and invoke one function that takes in a random string and returns the
+//following values: first character, last character, length of the string and
+//whether it begins with a vowel or not. (6pts)
+fun invoke(arg:String<Strings>){
+    val name= ("jane","james","joy","shiko")
+    for (i=0,i< string.lenght-1)
+
+}
+
+//2. Create a function that takes in a password as a parameter. For a password to
+//be valid it must meet the following conditions:
+//1. Must be at least 8 characters long
+//2. Must be at most 16 characters long
+//3. Must not be “password”
+//4. Must contain a digit
+//Your function should determine whether the password provided is valid or not.
+//The function returns true/false (7pts)
+fun password(password:String){
+    val password=("Joyce43mbiwanjik")
+
+
+}
+//
+//3. Create a function that prints out multiples of 6 and 8 between 1 and 1000. For
+//multiples of both 6 and 8 it should print out “Bingo!” (5pts)
+fun Multiples(){
+    var multiplication=(1,100)
+    multiplies
+
+}
+//4. Create a class CurrentAccount with the following attributes: account number,
+//account name, balance. It has the following functions:
+//a. deposit(amount: Double) - Increments the balance by the amount
+//deposited
+//b. withdraw(amount: Double) - Decrements the balance by the amount
+//withdrawn
+//c. details() - Prints out the account number and balance and name in
+
+class currentAccoount(var AccountNumber:Int,var AccountName:,var string ccountBalance:Double)
+,
+fun deposit(amount:Double){
+    deposited=1000
+    var addation=desposited
+    deposited.add
+}
+fun withdraw(amount:Double){
+    withdraw=500
+    var minus=deposited
+    withdraw.subtract
+
+}
+fun details(AccountNumber: Int,AccountName:String,AccountBalance:Double){
+
+
+}
+
+//this format: “Account number x with balance y is operated by z” (6pts)
+//5. Create another class SavingsAccount. It has the same functions and
+//attributes as the current account except for one attribute, withdrawals: Int.
+//Withdrawals is a counter variable that is used to keep track of how many
+//withdrawals have been made from the account in a year. The only other
+//difference is that the savings account withdraw() method first checks if the
+//number of withdrawals is less than 4 for it to allow one to withdraw money
+//from the account. It also increments the withdrawals attribute after a
+//successful
+
+class  SavingsAccount(var AccountNumber:Int,var AccountName:,var String ccountBalance:Double.var withdrawals:Int)
+
+fun deposit(amount:Double){
+    deposited=
+    var addation=desposited
+    deposited.add
+}
+fun withdrwa(amount:Double){
+    withdrwa=500
+    var minus=deposited
+    withdrwa.subtract
+
+}
+fun withdraw(){
+    var withdrawals=(300)
+
+}
